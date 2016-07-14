7050af6665cc07a4275ac908dad4b5230f2ab7ed
王佳欣欣欣 <1974740999@qq.com>
2016-07-14 17:59:54 +0800

    生产环境webpack.config修改

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -3,6 +3,8 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
+
 module.exports = {
     devtool: 'eval',
     entry: {
