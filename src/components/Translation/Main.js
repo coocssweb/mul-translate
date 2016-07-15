8ea5b8e436d71660b88976b4635f80a79ef82e51
王佳欣欣欣 <1974740999@qq.com>
2016-07-15 18:32:10 +0800

    main 太多参数传递改用 {...this.props}的方式

--- src/components/Translation/Main.js
+++ src/components/Translation/Main.js
@@ -22,37 +22,11 @@ var Main = React.createClass({
                 <Header />
                 <div className="main">
                     <div className="main-left">
-                        <TranslationInput value={this.props.wordInput}
-                                          language_list={this.props.language_list}
-                                          language_input={this.props.language_input}
-                                          language_output={this.props.language_output}
-                                          setLanguageInput={this.props.setLanguageInput}
-                                          setLanguageOutput={this.props.setLanguageOutput}
-                                          setWordInput={this.props.setWordInput}
-                                          getWordOutput={this.props.getWordOutput}
-                                          getAcceptance={this.props.getAcceptance}
-                                          setCurrentEdit={this.props.setCurrentEdit}
-                                          setAcceptance={this.props.setAcceptance}
-                                          clearWordOutput={this.props.clearWordOutput}
-                            />
-                        <TranslationResult value={this.props.acceptance}
-                                           currentEdit={this.props.currentEdit}
-                                           setCurrentEdit={this.props.setCurrentEdit}
-                                           setAcceptance={this.props.setAcceptance}
-                                           updateAcceptance={this.props.updateAcceptance}
-                            />
+                        <TranslationInput {...this.props} />
+                        <TranslationResult {...this.props}/>
                     </div>
                     <div className="main-right">
-                        <TranslationOutput result={this.props.wordOutput}
-                                           currentEdit={this.props.currentEdit}
-                                           currentDisplay={this.props.currentDisplay}
-                                           currentShowType={this.props.currentShowType}
-                                           setCurrentDisplay={this.props.setCurrentDisplay}
-                                           setCurrentShowType={this.props.setCurrentShowType}
-                                           setCurrentEdit={this.props.setCurrentEdit}
-                                           setWordOutput={this.props.setWordOutput}
-                                           updateAcceptance={this.props.updateAcceptance}
-                            />
+                        <TranslationOutput {...this.props} />
                     </div>
                 </div>
             </div>
